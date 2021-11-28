07e213fa18adb44994311f0a0b75686c3a3f9274
Anurag <singh.ask@outlook.com>
2021-11-28 20:07:23 +0530

    first commit

---
+++ mario/lib/homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.blue,
+            )),
+        Expanded(
+            flex: 1,
+            child: Container(
+                color: Colors.brown,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.brown[300],
+                          child: Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.brown[300],
+                          child: Icon(Icons.arrow_upward),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.brown[300],
+                          child: Icon(Icons.arrow_forward),
+                        ),
+                      )
+                    ]))),
+      ],
+    ));
+  }
+}
---
+++ mario/lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+import 'package:mario/homepage.dart';
+import 'homepage.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
